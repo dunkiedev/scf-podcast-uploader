77cfed0e8c71aa15ad5d4bca03818042d94fc287
Phil Dunkerley <dev@thedunkerleys.org.uk>
2017-03-15 22:23:56 +0000

    Adding simple log4net configuration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using log4net;
+using log4net.Config;
+using log4net.Repository;
 using ScfPodcastUploader.Domain;
 using ScfPodcastUploader.Domain.WordPress;
 using ScfPodcastUploader.Services;
@@ -8,26 +14,17 @@ namespace ScfPodcastUploader
 {
     public class Program
     {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(Program));
+
         public static void Main(string[] args)
         {
+            //configure log4net
+            ILoggerRepository defaultRepository = log4net.LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.ConfigureAndWatch(defaultRepository, new FileInfo("log4net.config"));
+
             IContainer container = Container.For<ScfPodcastUploaderRegistry>();
             Program program = container.GetInstance<Program>();
             program.Run();
-            //service.GetPost(2605).Wait();
-
-            // WordPressPost post = new WordPressPost
-            //     {
-            //         title = "Test post1",
-            //         status = "publish",
-            //         content = "Test content",
-            //     };
-            // bool isSuccess = service.AddPost(post);
-
-            //bool isSuccess = service.AddMedia2("/Users/phil/Documents/Shenley/SCF Podcast/SCF_2017-03-05.mp3");
-            //bool isSuccess = service.AddMedia2("/Users/phil/Documents/Shenley/SCF Podcast/SCF_2016-01-24.mp3");
-            //bool isSuccess = service.AddMedia("/Users/phil/Documents/Shenley/SCF Podcast/dummy3.mp3");
-
-            //Console.Out.WriteLine($"Success: {isSuccess}");
         }
         private readonly IPodcastService _podcastService;
 
